17f0065926e183cf69039ea95e6bfc8b4ef22540
Ayşe Oduncu <59964531+codergirll-droid@users.noreply.github.com>
2022-10-31 04:08:56 +0300

    E03 : Octaves Completed

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,12 +10,42 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistance, lacunarity, offset);
 
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
     }
 
+    private void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if(octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
 }
